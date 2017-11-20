fbe605d49d30d05d19bfc59bd8821833cbee4cc6
Andrew Zubko <andrew@applicature.com>
2017-11-20 03:36:10 +0200

    add abstract blockchain listener to core

--- packages/applicature.plugin-manager/index.js
+++ packages/applicature.plugin-manager/index.js
@@ -1,11 +1,16 @@
 const PluginManager = require('./src/plugin.manager');
 const AbstractPlugin = require('./src/plugin.abstract');
 const AbstractJob = require('./src/jobs/abstract.job');
+const AbstractBlockchainListener = require('./src/jobs/abstract.listener');
 const AbstractBlockchain = require('./src/services/blockchain/abstract');
 const AbstractIco = require('./src/services/ico/abstract.ico');
 
 const MultivestError = require('./src/error');
 
 module.exports = {
-    PluginManager, AbstractPlugin, AbstractJob, AbstractBlockchain, AbstractIco, MultivestError,
+    PluginManager, AbstractPlugin,
+    AbstractJob, AbstractBlockchainListener,
+    AbstractBlockchain,
+    AbstractIco,
+    MultivestError,
 };
