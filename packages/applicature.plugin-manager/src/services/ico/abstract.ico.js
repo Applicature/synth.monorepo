67fd1c2025ddb07f7025f0c80d69a8c44dd24843
Andrew Zubko <andrew@applicature.com>
2017-11-21 11:49:59 +0200

    update stats

--- packages/applicature.plugin-manager/src/services/ico/abstract.ico.js
+++ packages/applicature.plugin-manager/src/services/ico/abstract.ico.js
@@ -1,17 +1,22 @@
 class AbstractICO {
 // eslint-disable-next-line class-methods-use-this,no-unused-vars
-    logContribution(network, txHash, amount, params) {
-        // @TODO: add contribution to db
+    logContribution(network, uniqId, txHash, amount, params) {
+        // add contribution to db
     }
 
 // eslint-disable-next-line class-methods-use-this,no-unused-vars
-    handleExternalContribution(fromNetwork, fromTxHash, forAddress, amount, params) {
-        // @TODO: create transaction for execution
+    handleExternalContribution(uniqId, parentTx, icoAddressId, userAddress, tokens, status, at) {
+        // create transaction for execution
     }
 
 // eslint-disable-next-line class-methods-use-this,no-unused-vars
     listContributions(addresses) {
-        // @TODO: list all logged contributions
+        // list all logged contributions
+    }
+
+// eslint-disable-next-line class-methods-use-this
+    getStats() {
+        // get stats
     }
 }
 
