884a7be556b4beb02c00d44770a5d2fe704b76b2
Andrew Zubko <andrew@applicature.com>
2017-11-22 02:12:00 +0200

    add mostly used fields to key abstract class

--- packages/applicature.plugin-manager/src/services/kyc/abstract.kyc.js
+++ packages/applicature.plugin-manager/src/services/kyc/abstract.kyc.js
@@ -4,8 +4,8 @@ class AbstractKyc {
         this.id = id;
     }
 
-// eslint-disable-next-line class-methods-use-this,no-unused-vars
-    validate(userId, fields) {
+// eslint-disable-next-line class-methods-use-this,no-unused-vars,max-len
+    validate(userId, firstName, lastName, dateOfBirth, country, state, city, streetAddress, appartament, zipCode, fields) {
 
     }
 }
