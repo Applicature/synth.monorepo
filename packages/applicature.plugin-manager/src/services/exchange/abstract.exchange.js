8bbedc42acbca673cb830572ca62b874f61e86ee
Andrew Zubko <andrew@applicature.com>
2017-11-27 14:31:15 +0200

    extend getRate with value

--- packages/applicature.plugin-manager/src/services/exchange/abstract.exchange.js
+++ packages/applicature.plugin-manager/src/services/exchange/abstract.exchange.js
@@ -2,7 +2,7 @@ const MultivestError = require('../../error');
 
 class AbstractExchange {
 // eslint-disable-next-line class-methods-use-this,no-unused-vars
-    getRate(from, to) {
+    getRate(value, from, to) {
         throw new MultivestError('not-implemented');
     }
 }
