d57b5b01ba3bfe3ba5cb3bb2a2d38dd25781125b
Nikita Radetskiy <nikita.radetskiy@applicature.com>
2018-01-04 18:32:51 +0200

    data structures defined

--- packages/applicature.plugin-manager/src/services/blockchain/model.ts
+++ packages/applicature.plugin-manager/src/services/blockchain/model.ts
@@ -1,7 +1,40 @@
+import { BigNumber } from 'bignumber.js'; 
+
 export interface Block {
+    height: number;
+    hash: string;
+    parentHash?: string;
+    difficulty?: number;
+    nonce: any;
+    size: number;
+    time: Date;
+    network: string;
+    fee: BigNumber;
+    transactions: Transaction[];
+}
+
+export interface BitcoinBlock extends Block {
+
+}
+
+export interface EthereumBlock extends Block {
 
 }
 
 export interface Transaction {
-    
+    hash: string;
+    blockHash?: string;
+    blockHeight?: number;
+    fee: number;
+    from: Sender[];
+    to: Recipient[];
+}
+
+export interface Sender {
+    address: string;
+}
+
+export interface Recipient {
+    address: string;
+    amount: number;
 }
