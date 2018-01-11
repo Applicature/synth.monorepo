572e446b9a965b3e95a165c88f067633ccac0435
Nikita Radetskiy <nikita.radetskiy@applicature.com>
2018-01-11 15:40:54 +0200

    Dao fixes

--- packages/applicature.plugin-manager/src/structure/dao.ts
+++ packages/applicature.plugin-manager/src/structure/dao.ts
@@ -4,7 +4,7 @@ export abstract class Dao<T> {
     abstract getDaoId(): string;
     abstract get(needle: Partial<T>): Promise<T>;
     abstract list(needle: Partial<T>): Promise<T[]>;
-    abstract update(needle: Partial<T>, substitution: Partial<T>): Promise<any>;
-    abstract create(needle: T): Promise<any>;
-    abstract remove(needle: Partial<T>): Promise<any>; 
+    abstract update(needle: Partial<T>, substitution: Partial<T>): Promise<Partial<T>>;
+    abstract create(needle: T): Promise<T>;
+    abstract remove(needle: Partial<T>): Promise<Partial<T>>; 
 }
