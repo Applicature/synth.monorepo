acb6beb8617adec6c96f5cc73547d23df26f56ce
Nikita Radetskiy <nikita.radetskiy@applicature.com>
2018-01-11 16:23:56 +0200

    db scheme added to export

--- packages/applicature.plugin-manager/src/structure/index.ts
+++ packages/applicature.plugin-manager/src/structure/index.ts
@@ -1,6 +1,7 @@
 export * from './block';
 export * from './contribution';
 export * from './dao';
+export * from './db.scheme';
 export * from './ico.address';
 export * from './transaction';
 export * from './system';
