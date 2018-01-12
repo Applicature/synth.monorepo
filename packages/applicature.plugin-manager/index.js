5d2da35ed6261d4ad65bbad5f3c711a297576d20
Nikita Radetskiy <nikita.radetskiy@applicature.com>
2018-01-12 13:35:15 +0200

    index file updated

--- packages/applicature.plugin-manager/index.js
+++ packages/applicature.plugin-manager/index.js
@@ -3,12 +3,9 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./src/jobs"));
+__export(require("./src/entities"));
 __export(require("./src/services/crypt"));
-__export(require("./src/services/exchange"));
-__export(require("./src/services/ico"));
 __export(require("./src/services/kyc"));
 __export(require("./src/error"));
-__export(require("./src/structure"));
 __export(require("./src/plugin"));
 __export(require("./src/plugin.manager"));
