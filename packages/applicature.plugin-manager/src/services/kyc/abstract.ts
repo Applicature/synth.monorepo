8c242c21f088177d400dea637a1201ac88022ba5
Oleksiy Tkachenko <oleksandr.kulyna@applicature.com>
2018-01-22 17:59:45 +0200

    KYC update

--- packages/applicature.plugin-manager/src/services/kyc/abstract.ts
+++ packages/applicature.plugin-manager/src/services/kyc/abstract.ts
@@ -2,9 +2,8 @@ import { PluginManager } from '../../plugin.manager';
 import { KYCStatus } from './model';
 
 export abstract class KYCService<T> {
-    public status: KYCStatus;
 
     constructor(private pluginManager: PluginManager, private id: number) {}
 
-    public abstract validate(data: T): boolean;
+    public abstract validate(data: T): Promise<KYCStatus>;
 }
