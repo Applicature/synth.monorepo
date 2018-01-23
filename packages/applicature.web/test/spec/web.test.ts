86b192a69906f5253a5bc3bd2bfc9c134e2c1233
Ihor Fito <ihor.fito@applicature.com>
2018-01-23 16:51:44 +0200

    fix unit tests

--- packages/applicature.web/test/spec/web.test.ts
+++ packages/applicature.web/test/spec/web.test.ts
@@ -1,28 +1,28 @@
-import {expect} from 'chai';
-import {Web} from '../../src/web';
-import {Router} from 'express';
 import * as config from 'config';
+import * as express from 'express';
+import { expect } from 'chai';
+import { Web } from '../../src/web';
 
 describe('Web Plugin', () => {
-    it('should init web plugin', () => {
+    test('should init web plugin', () => {
         const web = new Web();
     });
-    it('should add Routes', () => {
-        const app = expres();
+    test('should add Routes', () => {
+        const app = express();
         const web = new Web(config, app);
-        const router = Router();
+        const router = express.Router();
         web.addRouter('one', router);
     });
-    it('should enable Router', () => {
-        const app = expres();
+    test('should enable Router', () => {
+        const app = express();
         const web = new Web(config, app);
-        const router = Router();
+        const router = express.Router();
         web.addRouter('one', router);
         web.enableRouter('one');
     });
-    it('should start Server', () => {
-        const app = expres();
+    test('should start Server', () => {
+        const app = express();
         const web = new Web(config, app);
-        web.startServer()
+        web.startServer();
     });
 });
