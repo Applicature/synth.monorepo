468d3b4ac9899888e23fda6f229ae430c7fc884c
Nikita Radetskiy <nikita.radetskiy@applicature.com>
2018-01-25 01:17:53 +0200

    tests for parseDecimals() added

--- packages/applicature.mongodb/src/mongodb.dao.ts
+++ packages/applicature.mongodb/src/mongodb.dao.ts
@@ -3,68 +3,14 @@ import { BigNumber } from 'bignumber.js';
 import { Collection, Db, Decimal128 } from 'mongodb';
 
 export abstract class MongoDBDao<T> implements Dao<T> {
-    protected collection: Collection<T>;
-    private db: Db;
 
-    constructor(db: Db) {
-        this.db = db;
-        this.collection = this.db.collection<T>(this.getCollectionName());
-    }
-
-    public abstract getDaoId(): string;
-
-    public abstract getCollectionName(): string;
-
-    public abstract getDefaultValue(): T;
-
-    public create(needle: Partial<T>) {
-        const fulfilled = Object.assign(this.getDefaultValue(), needle);
-        const parsed = this.parseDecimals('toMongo', fulfilled);
-        return this.collection
-            .insertOne(parsed)
-            .then<T>((result) => result.ops[0]);
-    }
-
-    public fill(needle: Array<Partial<T>>) {
-        const parsed = this.parseDecimals('toMongo', needle);
-        return this.collection
-            .insertMany(parsed)
-            .then<Array<T>>((result) => result.ops);
-    }
-
-    public get(needle: Partial<T>) {
-        return this.collection
-            .findOne(needle)
-            .then((item) => this.parseDecimals('fromMongo', item) as T);
-    }
-
-    public list(needle: Partial<T>) {
-        return this.collection
-            .find(needle)
-            .toArray()
-            .then((list) => this.parseDecimals('fromMongo', list) as Array<T>);
-    }
-
-    public update(needle: Partial<T>, substitution: Partial<T>) {
-        const parsed = this.parseDecimals('toMongo', substitution);
-        return this.collection
-            .updateMany(needle, { $set: parsed })
-            .then(() => needle);
-    }
-
-    public remove(needle: Partial<T>) {
-        return this.collection
-            .deleteMany(needle)
-            .then(() => needle);
-    }
-
-    public parseDecimals(type: 'toMongo' | 'fromMongo', data: any): any {
+    public static parseDecimals(type: 'toMongo' | 'fromMongo', data: any): any {
         if (typeof data !== 'object') {
             return data;
         }
 
         if (Array.isArray(data)) {
-            return data.map((item) => this.parseDecimals(type, item));
+            return data.map((item) => MongoDBDao.parseDecimals(type, item));
         }
 
         if (type === 'toMongo' && data instanceof BigNumber) {
@@ -79,10 +25,63 @@ export abstract class MongoDBDao<T> implements Dao<T> {
 
         if (keys.length) {
             return keys.reduce((prev, curr) => {
-                prev[curr] = this.parseDecimals(type, data[curr]);
+                prev[curr] = MongoDBDao.parseDecimals(type, data[curr]);
                 return prev;
             },
             {} as Hashtable<any>);
         }
+    }
+
+    protected collection: Collection<T>;
+
+    constructor(db: Db) {
+        this.collection = db.collection<T>(this.getCollectionName());
+    }
+
+    public abstract getDaoId(): string;
+
+    public abstract getCollectionName(): string;
+
+    public abstract getDefaultValue(): T;
+
+    public create(needle: Partial<T>) {
+        const fulfilled = Object.assign(this.getDefaultValue(), needle);
+        const parsed = MongoDBDao.parseDecimals('toMongo', fulfilled);
+        return this.collection
+            .insertOne(parsed)
+            .then<T>((result) => result.ops[0]);
+    }
+
+    public fill(needle: Array<Partial<T>>) {
+        const parsed = MongoDBDao.parseDecimals('toMongo', needle);
+        return this.collection
+            .insertMany(parsed)
+            .then<Array<T>>((result) => result.ops);
+    }
+
+    public get(needle: Partial<T>) {
+        return this.collection
+            .findOne(needle)
+            .then((item) => MongoDBDao.parseDecimals('fromMongo', item) as T);
+    }
+
+    public list(needle: Partial<T>) {
+        return this.collection
+            .find(needle)
+            .toArray()
+            .then((list) => MongoDBDao.parseDecimals('fromMongo', list) as Array<T>);
+    }
+
+    public update(needle: Partial<T>, substitution: Partial<T>) {
+        const parsed = MongoDBDao.parseDecimals('toMongo', substitution);
+        return this.collection
+            .updateMany(needle, { $set: parsed })
+            .then(() => needle);
+    }
+
+    public remove(needle: Partial<T>) {
+        return this.collection
+            .deleteMany(needle)
+            .then(() => needle);
     }
 }
