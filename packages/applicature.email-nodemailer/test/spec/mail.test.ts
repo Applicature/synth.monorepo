3a77fc0c706db76dd85e48c4d208972726e9a2d7
Andrew Zubko <andrew@applicature.com>
2018-02-09 17:26:18 +0200

    code style

--- packages/applicature.email-nodemailer/test/spec/mail.test.ts
+++ packages/applicature.email-nodemailer/test/spec/mail.test.ts
@@ -1,18 +1,17 @@
 import EmailService from '../../src/service';
-import {EmailOpts} from '@applicature/multivest.email.abstract/dist';
-import {EmailBodies} from '@applicature/multivest.email.abstract/src/service';
+import {EmailOpts, EmailBodies} from '@applicature/multivest.email.abstract/dist';
 
 describe('test', () => {
     it('should work', async () => {
         const service: EmailService = new EmailService();
         const emailOpts: EmailOpts = new EmailOpts({
-            from: "test@exmplae.com",
-            to: "test-to@exmplae.com",
-            cc: ["copy-1@example.com"],
-            replyTo: "reply-to@example.com",
-            inReplyTo: "in-reply-to",
-            subject: "test-subject",
-            body: new EmailBodies("<b>Hello world</b>", "Hello world!"),
+            from: 'test@exmplae.com',
+            to: 'test-to@exmplae.com',
+            cc: ['copy-1@example.com'],
+            replyTo: 'reply-to@example.com',
+            inReplyTo: 'in-reply-to',
+            subject: 'test-subject',
+            body: new EmailBodies('<b>Hello world</b>', 'Hello world!'),
         });
 
         const result = await service.sendEmail(emailOpts);
