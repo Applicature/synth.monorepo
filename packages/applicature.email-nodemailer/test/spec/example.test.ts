db788dc0b07c1ef2bb8fb74e5ed1538adeb70651
Andrew Zubko <andrew@applicature.com>
2018-02-09 17:24:49 +0200

    adding tests

--- packages/applicature.email-nodemailer/test/spec/example.test.ts
+++ packages/applicature.email-nodemailer/test/spec/example.test.ts
@@ -1,6 +1,24 @@
+import EmailService from '../../src/service';
+import {EmailOpts} from '@applicature/multivest.email.abstract/dist';
+import {EmailBodies} from '@applicature/multivest.email.abstract/src/service';
 
 describe('test', () => {
-    it('should work', () => {
+    it('should work', async () => {
+        const service: EmailService = new EmailService();
+        const emailOpts: EmailOpts = new EmailOpts({
+            from: "test@exmplae.com",
+            to: "test-to@exmplae.com",
+            cc: ["copy-1@example.com"],
+            replyTo: "reply-to@example.com",
+            inReplyTo: "in-reply-to",
+            subject: "test-subject",
+            body: new EmailBodies("<b>Hello world</b>", "Hello world!"),
+        });
+
+        const result = await service.sendEmail(emailOpts);
+
+        console.log(result);
+
         expect(1).toBe(1);
     });
 });
