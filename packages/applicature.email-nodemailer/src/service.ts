d6c1c9a8e4d391e3b24db863640dd6cc57c16d53
Andrew Zubko <andrew@applicature.com>
2018-02-09 16:56:58 +0200

    implement nodemail service

--- packages/applicature.email-nodemailer/src/service.ts
+++ packages/applicature.email-nodemailer/src/service.ts
@@ -1,40 +1,49 @@
-'use strict';
-const nodemailer = require('nodemailer');
+import {EmailOpts} from '@applicature/multivest.email.abstract/src/service';
+import * as nodemailer from 'nodemailer';
 
-// Generate test SMTP service account from ethereal.email
-// Only needed if you don't have a real mail account for testing
-nodemailer.createTestAccount((err, account) => {
+import { PluginManager } from '@applicature/multivest.core';
+import { EmailService } from '@applicature/multivest.email.abstract';
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: account.user, // generated ethereal user
-            pass: account.pass  // generated ethereal password
-        }
-    });
+import * as config from 'config';
 
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"Fred Foo 👻" <foo@example.com>', // sender address
-        to: 'bar@example.com, baz@example.com', // list of receivers
-        subject: 'Hello ✔', // Subject line
-        text: 'Hello world?', // plain text body
-        html: '<b>Hello world?</b>' // html body
-    };
+class NodeMailerEmailService extends EmailService {
+    private transporter: any;
 
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    constructor(pluginManager: PluginManager) {
+        super(pluginManager);
 
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    });
-});
+        this.transporter = nodemailer.createTransport(config.get('email.nodemailer'));
+    }
+
+    public sendEmail(emailOpts: EmailOpts): Promise<any> {
+        const mail = {
+            from: emailOpts.from,
+            to: emailOpts.to,
+
+            bcc: emailOpts.bcc,
+            cc: emailOpts.cc,
+
+            subject: emailOpts.subject,
+
+            html: emailOpts.body.HTMLBody,
+            text: emailOpts.body.textBody,
+
+            inReplyTo: emailOpts.inReplyTo,
+            replyTo: emailOpts.replyTo,
+
+            messageId: emailOpts.messageId,
+        };
+
+        return new Promise((resolve, reject) => {
+            this.transporter.sendMail(mail, (error: any, info: any) => {
+                // @TODO: map error & info
+
+                if (error) {
+                    return reject(error);
+                }
+
+                resolve(info);
+            });
+        });
+    }
+}
