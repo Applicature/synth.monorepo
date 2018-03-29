810cd199736e14a22287da2e9810255d38caa7b2
Andrew Zubko <andrew@applicature.com>
2018-03-29 20:01:06 +0300

    add test to render string

---
+++ packages/applicature.plugin-manager/test/spec/template.test.ts
@@ -0,0 +1,14 @@
+import {
+    TemplateService
+} from '../../src/services/template';
+
+describe('template', () => {
+
+    it('should render string', () => {
+        const templateService = new TemplateService(null);
+
+        const renderedString = templateService.renderFromString('Hello {{ name }}', {name: 'World'})
+
+        expect(renderedString).toEqual('Hello World');
+    });
+});
