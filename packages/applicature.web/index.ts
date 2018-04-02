563f70a4283250da7a0b5d027a28d9ab8c8569e6
Andrew Zubko <andrew@applicature.com>
2018-04-03 02:05:25 +0300

    update

--- packages/applicature.web/index.ts
+++ packages/applicature.web/index.ts
@@ -1,4 +1,5 @@
 export * from './src/plugin.web';
+export * from './src/pluginInterface';
 export * from './src/services/validation/validation.default.service';
 export * from './src/services/validation/validation.abstract.service';
 export * from './src/error';
