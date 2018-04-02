514b85c106233123b1848cdd36112a9db3a8c548
Andrew Zubko <andrew@applicature.com>
2018-04-03 00:46:56 +0300

    fix codestyle

--- packages/applicature.plugin-manager/test/spec/template.test.ts
+++ packages/applicature.plugin-manager/test/spec/template.test.ts
@@ -1,5 +1,5 @@
 import {
-    TemplateService
+    TemplateService,
 } from '../../src/services/template';
 
 describe('template', () => {
@@ -7,7 +7,7 @@ describe('template', () => {
     it('should render string', () => {
         const templateService = new TemplateService(null);
 
-        const renderedString = templateService.renderFromString('Hello {{ name }}', {name: 'World'})
+        const renderedString = templateService.renderFromString('Hello {{ name }}', {name: 'World'});
 
         expect(renderedString).toEqual('Hello World');
     });
