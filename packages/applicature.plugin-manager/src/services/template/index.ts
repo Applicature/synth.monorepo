684529074c06d19aa3889ab86f75413e11f391a2
Ihor Fito <ihor.fito@applicature.com>
2018-04-27 15:33:26 +0300

    test i18n

--- packages/applicature.plugin-manager/src/services/template/index.ts
+++ packages/applicature.plugin-manager/src/services/template/index.ts
@@ -1,9 +1,23 @@
 import { get as getConfig } from 'config';
 import * as fs from 'fs';
-import { compile, registerHelper, registerPartial } from 'handlebars';
+import { compile, registerHelper, registerPartial, SafeString } from 'handlebars';
 import { Service } from '../../entities/service';
 import { PluginManager } from '../../plugin.manager';
 import { Hashtable } from '../../structure';
+import * as i18next from 'i18next';
+
+var resources = {
+    dev: { translation: { 'EMAIL_FAQ_BODY': 'User Name: {{username}}<br>User Email: {{email}}<br>Question: {{question}}' } },
+    en: { translation: { 'EMAIL_FAQ_BODY': 'User Name: {{username}}<br>User Email: {{email}}<br>Question: {{question}}' } },
+    'en-US': { translation: { 'EMAIL_FAQ_BODY': 'User Name: {{username}}<br>User Email: {{email}}<br>Question: {{question}}' } }
+};
+
+i18next.init({ resStore: resources });
+
+registerHelper('i18next', (key: string, question: any) => {
+    const result = i18next.t(key, {question});
+    return new SafeString(result);
+});
 
 function walkSync(parentDir: string, directory: string, filelist: Array<Array<string>>) {
     directory = directory || '';
@@ -74,9 +88,7 @@ export class TemplateService extends Service {
 
         for (const file of templatesFiles) {
             const content = await this.getContent(file[0] + '/' +  file[1], file[2]);
-
             const compiledTemplate = compile(content);
-
             this.compiledTemplates[file[2]] = compiledTemplate;
         }
     }
